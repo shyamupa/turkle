3897233fb43ec3e74e6ddcfcf65d609fc5a88a43
Shyam Upadhyay <shyamupa@gmail.com>
2017-01-02 13:50:50 -0600

    modified to take order into account

--- annotated/agreement.py
+++ annotated/agreement.py
@@ -1,18 +1,36 @@
 # coding: utf-8
+import sys
 
-f=open('bhargav_hi.csv')
-g=open('shyam_hi.csv')
-h=open('bhargav_hi.csv')
-i=open('shyam_hi.csv')
-j=open('shyam_hi.csv')
-f.readline(),g.readline(),h.readline(),i.readline(),j.readline()
-lines=[(l1.strip(),l2.strip(),l3.strip(),l4.strip(),l5.strip()) for
-       l1,l2,l3,l4,l5 in zip(f,g,h,i,j)]
-count=0
-for line in lines:
-    l1,l2,l3,l4,l5=line
-    l1,l2,l3,l4,l5=l1.split(','),l2.split(','),l3.split(','),l4.split(','),l5.split(',')
-    o1,o2,o3,o4,o5=l1[0],l2[0],l3[0],l4[0],l5[0]
+def load_judgements(filepath):
+    f=open(filepath)
+    f.readline()
+    judgements=[]
+    for l in f:
+        parts=l.strip().split(",")
+        val=parts[0]
+        key=(parts[1],parts[2])
+        judgements.append((key,val))
+    return dict(judgements)
+
+# jj=load_judgements('shyam_hi.csv')
+# print(jj)
+# print(len(jj))
+# sys.exit(0)
+
+f=load_judgements('bhargav_hi.csv')
+g=load_judgements('shyam_hi.csv')
+h=load_judgements('bhargav_hi.csv')
+i=load_judgements('shyam_hi.csv')
+j=load_judgements('shyam_hi.csv')
+
+assert len(f.keys())==len(g.keys())
+# f.readline(),g.readline(),h.readline(),i.readline(),j.readline()
+# lines=[(l1.strip(),l2.strip(),l3.strip(),l4.strip(),l5.strip()) for
+#        l1,l2,l3,l4,l5 in zip(f,g,h,i,j)]
+
+for key in f.keys():
+    # l1,l2,l3,l4,l5=line
+    # l1,l2,l3,l4,l5=l1.split(','),l2.split(','),l3.split(','),l4.split(','),l5.split(',')
+    o1,o2,o3,o4,o5=f[key],g[key],h[key],i[key],j[key]
     agree_cnt=[o1,o2,o3,o4,o5].count("yes")
-    print(l1[1:],agree_cnt)
-# print(count)
+    print(key,agree_cnt)
