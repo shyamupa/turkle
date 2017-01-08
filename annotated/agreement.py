5ae763cc5e33d6740bda53ca641113067f73a38e
Shyam Upadhyay <shyamupa@gmail.com>
2017-01-08 08:42:11 -0600

    wip

--- annotated/agreement.py
+++ annotated/agreement.py
@@ -20,7 +20,7 @@ def load_judgements(filepath):
 f=load_judgements('bhargav_hi.csv')
 g=load_judgements('shyam_hi.csv')
 h=load_judgements('vyas_hi.csv')
-# i=load_judgements('shyam_hi.csv')
+i=load_judgements('shashank_hi.csv')
 # j=load_judgements('shyam_hi.csv')
 
 assert len(f.keys())==len(g.keys())
@@ -34,11 +34,13 @@ for key in f.keys():
     # l1,l2,l3,l4,l5=l1.split(','),l2.split(','),l3.split(','),l4.split(','),l5.split(',')
     # o1,o2,o3,o4,o5=f[key],g[key],h[key],i[key],j[key]
     # agree_cnt=[o1,o2,o3,o4,o5].count("yes")
-    o1,o2,o3=f[key],g[key],h[key]
-    agree_cnt=[o1,o2,o3].count("yes")
+    o1,o2,o3,o4=f[key],g[key],h[key],i[key]
+    agree_cnt=[o1,o2,o3,o4].count("yes")
     print(key,agree_cnt)
     key_counts.append((key,agree_cnt))
 
 key_counts=sorted(key_counts, key=lambda x: x[1])
-print([k for k in key_counts if k[1]==3])
-print(len([k for k in key_counts if k[1]==3]))
+for k in key_counts:
+    if k[1]==4:
+        print(k)
+print(len([k for k in key_counts if k[1]==4]))
